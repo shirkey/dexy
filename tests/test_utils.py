be070292cb20e9ed4a9d20a5ea74a5723d283a6e
shirkey <dev@shirkey.me>
2014-01-22 13:36:54 +0700

    added test of dexy.utils.tempdir functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,27 +5,38 @@ from nose.tools import raises
 from dexy.utils import s
 from dexy.utils import split_path
 from dexy.utils import iter_paths
+from dexy.utils import tempdir
+
+
+def test_temp_dir():
+    temp = tempdir()
+    temp.make_temp_dir()
+    temp.remove_temp_dir()
+
 
 def test_iter_path():
     full_path = "/foo/bar/baz"
 
     expected_paths = {
-            0 : '/',
-            1 : '/foo',
-            2 : '/foo/bar',
-            3 : '/foo/bar/baz'
-            }
+        0: '/',
+        1: '/foo',
+        2: '/foo/bar',
+        3: '/foo/bar/baz'
+    }
 
     for i, path in enumerate(iter_paths(full_path)):
         assert expected_paths[i] == path
+
 
 def test_split_path():
     path = "foo/bar/baz"
     assert split_path(path) == ['foo', 'bar', 'baz']
 
+
 def test_split_path_with_root():
     path = "/foo/bar/baz"
     assert split_path(path) == ['', 'foo', 'bar', 'baz']
+
 
 def test_s():
     text = """This is some text
@@ -33,6 +44,7 @@ def test_s():
     many lines and has
     indents at the start."""
     assert s(text) == 'This is some text which goes onto many lines and has indents at the start.'
+
 
 class InactiveDexyFilter(Filter):
     """
@@ -42,6 +54,7 @@ class InactiveDexyFilter(Filter):
 
     def is_active(self):
         return False
+
 
 @raises(SkipTest)
 def test_inactive_filters_skip():
